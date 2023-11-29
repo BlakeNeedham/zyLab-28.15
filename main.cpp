3c0dee37f5eb4601d612d397ad6e3e486ca58583
25BurnettA <152207480+25BurnettA@users.noreply.github.com>
2023-11-29 11:43:39 -0600

    Update main.cpp
    Comments on submission requirements

--- main.cpp
+++ main.cpp
@@ -3,3 +3,6 @@ using namespace std;
 int main(){
   return 0;
 }
+
+//Remember you can skip step #9 which will  leave you with 21 points in the lab submission box in zyLabs (you get 6 additional points from posting lab on github).
+//All group members must submit assignment through zyLabs (including the url to the github assignment) in order to get credit for the assignment. 
