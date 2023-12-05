136889a9b2295ba10b33ad6b62e1e446205ac155
BlakeNeedham <152338171+BlakeNeedham@users.noreply.github.com>
2023-12-05 17:02:16 -0600

    Update PlaylistNode.h
    Added all member functions, basically just did step 1

--- PlaylistNode.h
+++ PlaylistNode.h
@@ -1,1 +1,38 @@
+//headers
+#ifndef PLAYLISTNODE_H
+#define PLAYLISTNODE_H
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class PlaylistNode {
+    public:
+    // default constructor
+    PlaylistNode();
+
+    // parameterized constructor
+    PlaylistNode(string& id, string& name, string& artist, int length);
+
+    // accessors
+    string GetID();
+    string GetSongName();
+    string GetArtistName();
+    int GetSongLength();
+    PlaylistNode* GetNext();
+
+    // mutators
+    void InsertAfter(PlaylistNode* nodePtr);
+    void SetNext(PlaylistNode* nodePtr);
+
+    // print function
+    void PrintPlaylistNode();
+
+  private: 
+    string uniqueID;
+    string songName;
+    string artistName;
+    int songLength;
+    PlaylistNode* nextNodePtr;
+};
+#endif
