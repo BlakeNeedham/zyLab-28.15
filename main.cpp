c80831cb1fa2cf3c3a8abbe881a45cdce5311981
BlakeNeedham <152338171+BlakeNeedham@users.noreply.github.com>
2023-12-06 12:07:29 -0600

    Update main.cpp
    Added steps 3 4 5 to main

--- main.cpp
+++ main.cpp
@@ -3,13 +3,62 @@
 using namespace std;
 int main(){
 
-  string userTitle;
-  cout << "Enter playlist's title: " << endl;
-  cin >> userTitle;
-  cout << userTitle;
+    string playlistTitle;
+    string userChoice;
+    PlaylistNode* head = nullptr;
 
-  PlaylistNode::PrintMenu(userTitle);
-  return 0;
+    cout << "Enter playlist's title:" << endl;
+    cin >> playlistTitle;
+  
+    while (true) {
+        PrintMenu(playlistTitle);
+
+        cout << "Choose an option: ";
+        cin >> userChoice;
+       
+        if (userChoice == 'q') {
+            break;  
+        }else if(userChoice == "a" || "d" || "c" || "s" || "t" || "o"){
+             head = ExecuteMenu(userChoice, playlistTitle, head);
+        }
+    }
+
+    return 0;
+}
+
+void OutputFullPlaylist(PlaylistNode* head) {
+    if (head == nullptr) {
+        cout << "Playlist is empty" << endl;
+        return;
+    }
+
+    int position = 1;
+    PlaylistNode* current = head;
+    while (current != nullptr) {
+        cout << position << ".\n";
+        current->PrintPlaylistNode();
+        cout << endl;
+
+        current = current->GetNext();
+        position++;
+    }
+}
+
+PlaylistNode* ExecuteMenu(string choice, string playlistTitle, PlaylistNode* head) {
+    switch (choice) {
+
+        case "o": {
+            cout << playlistTitle << " - OUTPUT FULL PLAYLIST" << endl;
+            OutputFullPlaylist(head);
+            break;
+        }
+
+        default: {
+            cout << "Invalid choice. Please try again." << endl;
+        }
+    }
+
+    return head;
 }
 
 //Remember you can skip step #9 which will  leave you with 21 points in the lab submission box in zyLabs (you get 6 additional points from posting lab on github).
