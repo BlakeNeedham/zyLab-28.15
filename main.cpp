625dd39de0a6a1806b0d0c704ef8d3f9cab614e6
BlakeNeedham <152338171+BlakeNeedham@users.noreply.github.com>
2023-12-06 12:40:04 -0600

    Update main.cpp
    Updating from zylabs work

--- main.cpp
+++ main.cpp
@@ -1,34 +1,13 @@
+//URL for github repository https://github.com/BlakeNeedham/zyLab-28.15.git
+//Team member 1 - Alyssa Burnett
+//Team member 2 - Blake Needham
 #include <iostream>
 #include "PlaylistNode.h"
 using namespace std;
-int main(){
-
-    string playlistTitle;
-    string userChoice;
-    PlaylistNode* head = nullptr;
-
-    cout << "Enter playlist's title:" << endl;
-    cin >> playlistTitle;
-  
-    while (true) {
-        PrintMenu(playlistTitle);
-
-        cout << "Choose an option: ";
-        cin >> userChoice;
-       
-        if (userChoice == 'q') {
-            break;  
-        }else if(userChoice == "a" || "d" || "c" || "s" || "t" || "o"){
-             head = ExecuteMenu(userChoice, playlistTitle, head);
-        }
-    }
-
-    return 0;
-}
 
 void OutputFullPlaylist(PlaylistNode* head) {
     if (head == nullptr) {
-        cout << "Playlist is empty" << endl;
+        cout << "Playlist is empty" << endl << endl;
         return;
     }
 
@@ -44,10 +23,10 @@ void OutputFullPlaylist(PlaylistNode* head) {
     }
 }
 
-PlaylistNode* ExecuteMenu(string choice, string playlistTitle, PlaylistNode* head) {
+PlaylistNode* ExecuteMenu(char choice, string playlistTitle, PlaylistNode* head) {
     switch (choice) {
 
-        case "o": {
+        case 'o': {
             cout << playlistTitle << " - OUTPUT FULL PLAYLIST" << endl;
             OutputFullPlaylist(head);
             break;
@@ -60,6 +39,46 @@ PlaylistNode* ExecuteMenu(string choice, string playlistTitle, PlaylistNode* hea
 
     return head;
 }
+
+void PrintMenu(string playlistTitle) {
+    cout << playlistTitle << " PLAYLIST MENU" << endl;
+    cout << "a - Add song" << endl;
+    cout << "d - Remove song" << endl;
+    cout << "c - Change position of song" << endl;
+    cout << "s - Output songs by specific artist" << endl;
+    cout << "t - Output total time of playlist (in seconds)" << endl;
+    cout << "o - Output full playlist" << endl;
+    cout << "q - Quit" << endl << endl;
+}
+
+int main(){
+
+    string playlistTitle;
+    char userChoice;
+    PlaylistNode* head = nullptr;
+
+    cout << "Enter playlist's title:" << endl << endl;
+    getline(cin, playlistTitle);
+    
+    while (true) {
+        PrintMenu(playlistTitle);
+
+        cout << "Choose an option:" << endl;
+        cin >> userChoice;
+       
+        if (userChoice == 'q') {
+            break;  
+        }else if(userChoice == 'a' || 'd' || 'c' || 's' || 't' || 'o'){
+             head = ExecuteMenu(userChoice, playlistTitle, head);
+        }
+    }
+
+    return 0;
+}
+
+
+
+
 
 //Remember you can skip step #9 which will  leave you with 21 points in the lab submission box in zyLabs (you get 6 additional points from posting lab on github).
 //All group members must submit assignment through zyLabs (including the url to the github assignment) in order to get credit for the assignment. 
