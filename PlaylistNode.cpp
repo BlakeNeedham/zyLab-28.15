d389329fa5144b1920bcafe0c7f9f3cea5247346
25BurnettA <152207480+25BurnettA@users.noreply.github.com>
2023-12-07 03:53:37 -0600

    Update PlaylistNode.cpp

--- PlaylistNode.cpp
+++ PlaylistNode.cpp
@@ -4,12 +4,22 @@
 #include <iostream>
 #include <string>
 
-// defualt constructor - sets the values to basically nothing
-PlaylistNode::PlaylistNode() : uniqueID("none"), songName("none"), artistName("none"), songLength(0), nextNodePtr(nullptr) {}
 
-// parameterized constructor - sets the actual values of these to the inputs
-PlaylistNode::PlaylistNode(string& id, string& name, string& artist, int length)
-    : uniqueID(id), songName(name), artistName(artist), songLength(length), nextNodePtr(nullptr) {}
+PlaylistNode::PlaylistNode(){
+    uniqueID = "none";
+    songName = "none";
+    artistName = "none";
+    songLength = 0;
+    nextNodePtr = nullptr;
+}
+
+PlaylistNode::PlaylistNode(string id, string name, string artist, int length){
+    uniqueID = id;
+    songName = name;
+    artistName = artist;
+    songLength = length;
+    nextNodePtr = nullptr;
+}
 
 // accessors
 string PlaylistNode::GetID() { 
@@ -37,6 +47,67 @@ void PlaylistNode::InsertAfter(PlaylistNode* nodePtr) {
     nodePtr->nextNodePtr = temp;
 }
 
+void PlaylistNode::insertAtEnd(PlaylistNode* newNode){
+    if(this->nextNodePtr == nullptr){
+        this->nextNodePtr=newNode;
+        return;
+    }
+
+    PlaylistNode* tmp = this->nextNodePtr;
+
+    while(tmp->nextNodePtr != nullptr){
+        tmp = tmp->nextNodePtr;
+    }
+    tmp->nextNodePtr = newNode;
+}
+
+void PlaylistNode::removeByID(string Id){
+    PlaylistNode* tmp = this->nextNodePtr;
+    PlaylistNode* prev_temp = this;
+
+    while(tmp->GetID() != Id && tmp!=nullptr){
+        prev_temp = tmp;
+        tmp = tmp->nextNodePtr;
+    }
+    if(tmp == nullptr){
+        return;
+    }
+    PlaylistNode* tmpNext = tmp->nextNodePtr;
+    prev_temp->nextNodePtr = tmpNext;
+    cout<<"\""<<tmp->GetSongName()<<"\""<<" removed."<< "\n" << endl;
+    delete tmp;
+
+}
+
+void PlaylistNode::printByArtist(string aName){
+    PlaylistNode* tmp = this->nextNodePtr;
+    int counter = 1;
+
+    while(tmp != nullptr){
+        if(tmp->GetArtistName()==aName){
+            cout<<counter<<'.'<<endl;
+            tmp->PrintPlaylistNode();
+            cout<<endl;
+
+        }
+       
+        counter++;
+        tmp = tmp->nextNodePtr;
+    }
+}
+
+void PlaylistNode::GetTime(){
+    PlaylistNode* tmp = this->nextNodePtr;
+    int tt =0;
+    while(tmp != nullptr){
+        tt += tmp->GetSongLength();
+        tmp = tmp->nextNodePtr;
+    }
+    cout <<"Total time: "<<tt<< " seconds"<<endl<<endl;
+
+}
+
+
 //setting what the next node is
 void PlaylistNode::SetNext(PlaylistNode* nodePtr) {
     nextNodePtr = nodePtr;
@@ -49,4 +120,3 @@ void PlaylistNode::PrintPlaylistNode() {
     cout << "Artist Name: " << GetArtistName() << endl;
     cout << "Song Length (in seconds): " << GetSongLength() << endl;
     }
-
